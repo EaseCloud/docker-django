7adb6b1653eba675768b8503f9b9c637aba4ebf0
Alfred Huang <57082212@qq.com>
2016-10-31 09:00:04 +0800

    Adding general edition branch

--- build.sh
+++ build.sh
@@ -1,2 +1,14 @@
-pip install --upgrade pip
-pip install gunicorn django greenlet eventlet
+#!/usr/bin/env bash
+
+apt-get update
+
+apt-get install -y \
+    python3.5-dev \
+    libffi-dev libxml2-dev libxslt1-dev \
+    openssl libssl-dev \
+    {gcc,g++}-arm-linux-gnueabi{,hf}
+
+wget https://bootstrap.pypa.io/get-pip.py
+python3.5 get-pip.py
+
+pip3.5 install -r requirements.txt
--- startup.sh
+++ startup.sh
@@ -1,15 +1,15 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -e
 
 echo $PROJECT
 
-if [ ! -e ./manage.py ]; then
-    django-admin startproject $PROJECT .
-fi
+#if [ ! -e ./manage.py ]; then
+#    django-admin startproject $PROJECT .
+#fi
 
-if [ -f ./requirements.txt ]; then
-    pip install -r requirements.txt
-fi
+#if [ -f ./requirements.txt ]; then
+#    pip install -r requirements.txt
+#fi
 
 gunicorn -b 0.0.0.0:8000 -w 4 -k eventlet $PROJECT.wsgi
