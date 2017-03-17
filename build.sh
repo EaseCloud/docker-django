a9693d1b6bc4db7fe5ae95551efcad4e7562b33a
fish-ball <57082212@qq.com>
2017-03-17 17:39:00 +0800

    apt-get 包有问题

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ apt-get install -y \
     python3-dev \
     libffi-dev libxml2-dev libxslt1-dev \
     openssl libssl-dev \
-    {gcc,g++}-arm-linux-gnueabi{,hf}
+#    {gcc,g++}-arm-linux-gnueabi{,hf}
 
 wget https://bootstrap.pypa.io/get-pip.py
 python get-pip.py
