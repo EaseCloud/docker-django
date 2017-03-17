748b484850954fe4e9d735dda796bd85136e3dd6
fish-ball <57082212@qq.com>
2017-03-17 17:38:33 +0800

    apt-get 包有问题

--- build.sh
+++ build.sh
@@ -4,10 +4,10 @@ apt-get update
 
 apt-get install -y \
     cron \
-    python3.5-dev \
+    python3-dev \
     libffi-dev libxml2-dev libxslt1-dev \
     openssl libssl-dev \
-    {gcc,g++}-arm-linux-gnueabi{,hf}
+    #{gcc,g++}-arm-linux-gnueabi{,hf}
 
 wget https://bootstrap.pypa.io/get-pip.py
 python get-pip.py
