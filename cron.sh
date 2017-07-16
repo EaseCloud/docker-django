bbbe4eea784ba1008f354dc298983e568455a656
Alfred Huang <57082212@qq.com>
2017-07-16 18:33:53 +0800

    Update cron.sh

--- cron.sh
+++ cron.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /var/app
-python manage.py runcrons >> /var/log/django_cron.log 2>> /var/log/django_cron.err.log
+pyp3 manage.py runcrons >> /var/log/django_cron.log 2>> /var/log/django_cron.err.log
