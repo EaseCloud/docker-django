aee1ba56277f1deec3670a6bcf4ed5cdaf52aeb2
fish-ball <57082212@qq.com>
2018-05-02 11:21:20 +0800

    upgrade to django 3.2.2

--- startup.sh
+++ startup.sh
@@ -2,15 +2,4 @@
 
 set -e
 
-#echo $PROJECT
-
-#if [ ! -e ./manage.py ]; then
-#    django-admin startproject $PROJECT .
-#fi
-
-#if [ -f ./requirements.txt ]; then
-#    pip install -r requirements.txt
-#fi
-
-gunicorn -b 0.0.0.0:8000 -w $WORKERS --reload $PROJECT.wsgi
-#python manage.py runserver 0.0.0.0:8000
+python manage.py runserver 0.0.0.0:8000
