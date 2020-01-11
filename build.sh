dd2caa267a516397a4116839ef9dedd2fb17e7e8
fish-ball <57082212@qq.com>
2020-01-11 23:14:53 +0800

    fix libav missing

--- build.sh
+++ build.sh
@@ -5,13 +5,12 @@ apt-get update
 # TODO: faac missing
 
 apt-get install -y \
-    cron \
-    openssl vim \
-    vorbis-tools lame flac mpg123 libav-tools faad \
+    cron openssl vim \
+    # vorbis-tools lame flac mpg123 libav-tools faad \
     python3-tk python3-dev libtiff5-dev libffi-dev \
     libxml2-dev libxslt1-dev libssl-dev zlib1g-dev \
     liblcms2-dev libwebp-dev tcl8.6-dev tk8.6-dev \
-    unixodbc unixodbc-dev wkhtmltopdf
+    # unixodbc unixodbc-dev wkhtmltopdf
 
 # compat avconv instead of ffmpeg
 # link: https://wiki.debian.org/ffmpeg
