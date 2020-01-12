c7164a376ef9e7f087880ceb039ce80a5e9101f9
fish-ball <57082212@qq.com>
2020-01-12 10:40:54 +0800

    更新了包安装脚本，避免被错误卡主

--- build.sh
+++ build.sh
@@ -4,13 +4,7 @@ apt-get update
 
 # TODO: faac missing
 
-apt-get install -y \
-    cron openssl vim \
-    # vorbis-tools lame flac mpg123 libav-tools faad \
-    python3-tk python3-dev libtiff5-dev libffi-dev \
-    libxml2-dev libxslt1-dev libssl-dev zlib1g-dev \
-    liblcms2-dev libwebp-dev tcl8.6-dev tk8.6-dev \
-    # unixodbc unixodbc-dev wkhtmltopdf
+apt-get install -y `cat apt-requirements.txt`
 
 # compat avconv instead of ffmpeg
 # link: https://wiki.debian.org/ffmpeg
@@ -19,6 +13,6 @@ ln /usr/bin/avconv /usr/bin/ffmpeg
 wget https://bootstrap.pypa.io/get-pip.py
 python get-pip.py
 
-pip install -r /var/requirements.txt
+pip install `cat /var/requirements.txt`
 
 echo "*/1 * * * * root PYTHONIOENCODING=UTF-8 /var/cron.sh" >> /etc/crontab
